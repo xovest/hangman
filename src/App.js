250b7d89006521856fcacc61e8a4ecb360e46206
xovest <79008917+xovest@users.noreply.github.com>
2021-11-18 13:47:55 +0200

    comps

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import './App.css';
 import Figure from './components/Figure';
 import Header from './components/Header';
+import Notification from './components/Notification';
+import Popup from './components/Popup';
+import Word from './components/Word';
+import WrongLetters from './components/WrongLetters';
 
 function App() {
   return (
@@ -8,6 +12,10 @@ function App() {
       <Header />
       <div className="game-container">
         <Figure />
+        <WrongLetters />
+        <Word />
+        <Popup />
+        <Notification />
       </div>
     </>
   );
