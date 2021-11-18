243b0b6f4725b68fd448ae1a16766df1df4deb83
xovest <79008917+xovest@users.noreply.github.com>
2021-11-18 13:43:42 +0200

    got the main parts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Figure from './components/Figure';
 import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Header />
+      <div className="game-container">
+        <Figure />
+      </div>
     </>
   );
 }
