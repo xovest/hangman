0141dd938c2247af724bf020bd0826133b2b8b48
xovest <79008917+xovest@users.noreply.github.com>
2021-11-18 14:22:02 +0200

    i can write

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Figure from './components/Figure';
 import Header from './components/Header';
@@ -6,14 +7,50 @@ import Popup from './components/Popup';
 import Word from './components/Word';
 import WrongLetters from './components/WrongLetters';
 
+const words = ['strawberry', 'friendship', 'everything', 'appreciate', 'motivation'];
+
+let selectedWord = words[Math.floor(Math.random() * words.length)];
+
 function App() {
+  const [playable, setPlayable] = useState(true);
+  const [correctLetters, setCorrectLetters] = useState([]);
+  const [wrongLetters, setWrongLetters] = useState([]);
+  
+  useEffect(() => {
+    const handleKeydown = e => {
+      const { key, keyCode } = e;
+      if (playable) { //we can play:D
+        if (keyCode >= 65 && keyCode <= 90) {
+          const letter = key.toLowerCase();
+    
+          if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+              setCorrectLetters(currLetters => [...currLetters, letter]);
+            } else {
+              //notif
+            }
+          } else {
+            if (!wrongLetters.includes(letter)) {
+              setWrongLetters(wrongLetters => [...wrongLetters, letter]);
+            } else {
+              //notif
+            }
+          }
+        }
+      }
+    };
+
+    window.addEventListener('keydown', handleKeydown);
+    return () => window.removeEventListener('keydown', handleKeydown);
+  }, [correctLetters, wrongLetters, playable]);
+  
   return (
     <>
       <Header />
       <div className="game-container">
         <Figure />
-        <WrongLetters />
-        <Word />
+        <WrongLetters wrongLetters={wrongLetters} />
+        <Word selectedWord={selectedWord} correctLetters={correctLetters} />
         <Popup />
         <Notification />
       </div>
--- src/components/Word.js
+++ src/components/Word.js
@@ -1,8 +1,16 @@
 import React from 'react';
 
-const Word = () => {
+const Word = ({ selectedWord, correctLetters }) => {
   return (
-    <div class="word" id="word"></div>
+    <div className="word">
+      {selectedWord.split('').map((letter, index) => {
+        return (
+          <span className="letter" key={index}>
+            {correctLetters.includes(letter) ? letter : ''}
+          </span>
+        );
+      })}
+    </div>
   );
 }
 
