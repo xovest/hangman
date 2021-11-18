28a3f0002e3274128b8bd297011f8e5de3f8cfd8
xovest <79008917+xovest@users.noreply.github.com>
2021-11-18 13:39:03 +0200

    setting up and header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>Guess the word - just type a letter</p>
+    </>
+  );
+}
+
+export default Header;
